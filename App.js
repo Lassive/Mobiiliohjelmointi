86b4950f3e09265cd1cc65521dc07eb4d7e57d6f
Lassive <lassive9@gmail.com>
2024-01-23 12:25:52 +0200

    Ensimmäinen palautus

--- App.js
+++ App.js
@@ -1,11 +1,34 @@
+import { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, Button, Alert, TextInput, Text } from 'react-native';
 
 export default function App() {
+  const [message, setMessage] = useState('');
+
+  const showAlert = () => {
+    Alert.alert('Moi!', 'Syötit tekstin: ' + message);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Result: </Text>
+
+      <TextInput style={{ 
+        width: 200, 
+        borderColor: 'black', 
+        borderWidth: 1}} 
+        onChangeText={text => setMessage(text)}
+      />
+      
+
+      <TextInput style={{ 
+        width: 200, 
+        borderColor: 'black', 
+        borderWidth: 1}} 
+        onChangeText={text => setMessage(text2)}
+      />
+
+      <Button title="Press me" onPress={showAlert} color='black' />
     </View>
   );
 }
@@ -13,7 +36,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'cyan',
     alignItems: 'center',
     justifyContent: 'center',
   },
